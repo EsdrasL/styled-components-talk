d2dad0ef3e346cf6de7b354f1572cb867e49e51d
Esdras Lemos <esdras.lemos07@gmail.com>
2020-02-03 21:25:08 -0300

    Updates

---
+++ precache-manifest.3802dd4c35567de69c61565d4a93d4e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f1dcabf9545da5ca5797e25316e6c2",
+    "url": "/styled-components-talk/index.html"
+  },
+  {
+    "revision": "39a7ec90469faaecbd91",
+    "url": "/styled-components-talk/static/css/main.b34475ec.chunk.css"
+  },
+  {
+    "revision": "12850085f54d71504f6e",
+    "url": "/styled-components-talk/static/js/2.280028ab.chunk.js"
+  },
+  {
+    "revision": "66f1945c2870f00beda8a179832bf292",
+    "url": "/styled-components-talk/static/js/2.280028ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39a7ec90469faaecbd91",
+    "url": "/styled-components-talk/static/js/main.903754e3.chunk.js"
+  },
+  {
+    "revision": "a0367537addd02faaee8",
+    "url": "/styled-components-talk/static/js/runtime-main.6fd515fc.js"
+  },
+  {
+    "revision": "f8c8386999009ab4502f576ce568dc1e",
+    "url": "/styled-components-talk/static/media/cool-outline.f8c83869.svg"
+  },
+  {
+    "revision": "bdfe356074ebea94822db3969d062194",
+    "url": "/styled-components-talk/static/media/mobills.bdfe3560.svg"
+  },
+  {
+    "revision": "6d94f33ca2440979e0ba969184403335",
+    "url": "/styled-components-talk/static/media/styled.6d94f33c.png"
+  },
+  {
+    "revision": "153d236c83f317a20438a4dd178d69f1",
+    "url": "/styled-components-talk/static/media/tagged-template-literals.153d236c.png"
+  }
+]);
